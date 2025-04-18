c72c56eb17368ede62d0d3b9c2fe42bf8e2f32e7
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2025-04-18 16:58:16 +0100

    Update loft.pl

--- Examples/loft.pl
+++ Examples/loft.pl
@@ -5,8 +5,8 @@ use CAD::OpenSCAD;
 use CAD::OpenSCAD::Math;
 use CAD::OpenSCAD::Loft;
 
-my $Math=new Math;
-my $scad=new SCAD(preview=>1);
+my $Math=new CAD::OpenSCAD::Math;
+my $scad=new OpenSCAD(preview=>1);
 my $profile=[[-1,1],[1,0.5],[1.75,0.25],[1.75,-0.25],[1,-0.5],[-1,-1]];
 my $lt = new Loft(scad=>$scad);
 $lt->helix("loft1",$profile,4,30,3,.1,.1);
